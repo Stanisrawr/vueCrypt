279601d3053b0cbacdec34201d9302a6c6747b36
Stanisrawr <stlesk@GMAIL.COM>
2017-09-13 14:30:35 -0400

    yay images

--- static/js/app.js
+++ static/js/app.js
@@ -5,19 +5,14 @@
  */
 
 // set up for apis
- let CRYPTOCOMPARE_API_URI="https://www.cryptocompare.com";
+ let CRYPTOCOMPARE_API_URI="https://www.cryptocompare.com/api/data/coinlist/";
  let COINMARKET_API_URI="https://api.coinmarketcap.com";
 
  let updateInterval=60*1000;
 
  let app=new Vue
- ({
-
-     created:function()
-       {
-           console.log("ran");
-          this.getCoinData();
-       },
+ ({ 
+        
     el:"#app",
     data:
     {
@@ -30,63 +25,65 @@
              * Load up all cryptocurrency data.  This data is used to find what logos
              * each currency has, so we can display things in a friendly way.
           */ 
-       getCoinData:function()
-       {
-            let self=this;
-            
-            axios.get(COINMARKET_API_URI+"/v1/ticker/?limit=10")
-              .then((resp)=>{
-                  this.coins=resp.data;
-              })
-              .catch((err)=>{
-                  console.error(err);
-              });  
+        getCoins: function()
+        {
+            let self = this;
 
-       },
+            axios.get(COINMARKET_API_URI + "/v1/ticker/?limit=10")
+                .then((resp) => {
+                this.coins = resp.data;
+                })
+                .catch((err) => {
+                console.error(err);
+                });
+            },
           /**
              * Get the top 10 cryptocurrencies by value.  This data is refreshed every 5
              * minutes by the backing API service.
           */
-       /* getCoins:function()
-       {
-            let self=this;
-            
-            axios.get(CRYPTOCOMPARE_API_URI+"/v1/ticker/?limit=10")
-              .then((resp)=>{
-                  this.coinData=resp.data;
-                  this.getCoins();
-              })
-              .catch((err)=>{
-                  this.getCoins();
-                  console.error(err);
-              });  
-
-
-       }, */
+        getCoinData: function() {
+      let self = this;
+        axios.get("http://cors.io/?u="+CRYPTOCOMPARE_API_URI)
+        .then((resp) => {
+          this.coinData = resp.data.Data;
+          this.getCoins();
+        })
+        .catch((err) => {
+          this.getCoins();
+          console.error(err);
+        });
+    },
           /**
              * Given a cryptocurrency ticket symbol, return the currency's logo
              * image.
           */
-      /*  getCoinImage:function(symbol)
+        getCoinImage:function(symbol)
        { 
-           console.log(CRYPTOCOMPARE_API_URI + this.coinData[symbol].ImageUrl);
-            return CRYPTOCOMPARE_API_URI + this.coinData[symbol].ImageUrl;
+           symbol = (symbol === "MIOTA" ? "IOT" : symbol);
+           symbol = (symbol === "VERI" ? "VRM" : symbol);
+            return "https://www.cryptocompare.com" +  this.coinData[symbol].ImageUrl;
                
-       }, */
+       }, 
 
        getColor: (num) => 
        {
             return num > 0 ? "color:green;" : "color:red;";
        },
+
+       
        
       
 
-    }
+    },
 
-    
+     created:function()
+       {
+           console.log("ran");
+          this.getCoinData();
+       }
 
  });
 
  setInterval(() => {
-  app.getCoinData();
+  app.getCoins();
 }, updateInterval);
